2b885c9fdf02e9c476c9c72c84425716811edb14
DarkLich <lich123good@gmail.com>
2025-03-24 01:46:15 +0800

    ch3

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_times: [0; 500],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,6 +136,20 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+
+    /// save syscall times
+    fn save_syscall_times(&self,id:usize){
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_times[id]+=1;
+
+    }
+    /// get syscall_times
+    fn get_syscall_times(&self,id:usize) -> isize{
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_times[id]
+    }
 }
 
 /// Run the first task in task list.
@@ -168,4 +183,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// interface to record the systimes
+pub fn save_syscall_times(id:usize){
+    TASK_MANAGER.save_syscall_times(id);
+}
+
+/// getsystimes
+pub fn get_syscall_times(id:usize) -> isize{
+    TASK_MANAGER.get_syscall_times(id)
 }
